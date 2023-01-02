76f28dd776966ff211f9c363613e482767539ea2
Jason <markjasongalang3@gmail.com>
2023-01-02 23:19:46 +0800

    implemented update modal

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 // Variable declarations
 const animeForm = document.querySelector('#anime-form');
+
+// New anime input
 const titleInput = document.querySelector('#title');
 const episodesInput = document.querySelector('#episodes');
-const statusInput = document.querySelector('#status');
 const scoreInput = document.querySelector('#score');
+
 const animeContainer = document.querySelector('#anime-container');
 const msg = document.querySelector('#msg');
 const tableBody = document.querySelector('tbody');
+
+// Modals
+const updateModal = document.querySelector('#updateModal');
 const removeModal = document.querySelector('#removeModal');
+
+// Update modal components
+const updateTitleInput = document.querySelector('#updateTitleInput');
+const updateEpisodesInput = document.querySelector('#update-episodes-input');
+const updateScoreInput = document.querySelector('#update-score-input');
+const saveUpdateBtn = document.querySelector('#save-update-btn');
+const cancelUpdateBtn = document.querySelector('#cancel-update-btn');
+
+// Remove modal components
 const yesRemoveBtn = document.querySelector('#yes-remove-btn');
 const cancelRemoveBtn = document.querySelector('#cancel-remove-btn');
+
 let currentRow;
 
 const animeList = [
@@ -76,18 +91,24 @@ const animeList = [
 ];
 
 // Pre-load anime list
-if(animeList.length > 0) {
+if (animeList.length > 0) {
   updateAnimeList();
 }
 
 // Generate data for score options
-for(let i = 0; i < 10; ++i) {
+for (let i = 0; i < 10; ++i) {
   const option = document.createElement('option');
   option.innerHTML = `${i + 1}`;
+
+  const option2 = document.createElement('option');
+  option2.innerHTML = `${i + 1}`;
+
   scoreInput.appendChild(option);
+  updateScoreInput.appendChild(option2);
 }
 
 // Initialize dropdown value (score)
+updateScoreInput.value = 5;
 scoreInput.value = 5;
 
 // Submitting a new anime
@@ -95,7 +116,7 @@ animeForm.addEventListener('submit', addAnime);
 function addAnime(e) {
   e.preventDefault();
 
-  if(titleInput.value === '' || episodesInput.value === '' || statusInput.value === '' || scoreInput.value === '') {
+  if (titleInput.value === '' || episodesInput.value === '' || scoreInput.value === '') {
     hideSuccess();
     showError();
     setTimeout(() => hideError(), 3000);
@@ -106,7 +127,7 @@ function addAnime(e) {
 
     // Capitalize each word of the title
     const words = titleInput.value.split(" ");
-    for(let i = 0; i < words.length; i++) {
+    for (let i = 0; i < words.length; i++) {
       words[i] = words[i][0].toUpperCase() + words[i].substr(1);
     }
 
@@ -121,17 +142,16 @@ function addAnime(e) {
 
     titleInput.value = '';
     episodesInput.value = '';
-    statusInput.value = '';
     scoreInput.value = '';
   }
 }
 
 function wordToHash(word) {
   let hash = 0, n = word.length;
-  if(n == 0) {
+  if (n == 0) {
     return hash;
   }
-  for(let i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++) {
     letter = word.charCodeAt(i);
     hash = ((hash << 5) - hash) + letter;
     hash = hash & hash;
@@ -165,27 +185,34 @@ function hideSuccess() {
 
 function updateAnimeList() {
   tableBody.innerHTML = '';
-  for(let i = animeList.length - 1; i >= 0; i--) {
+  for (let i = animeList.length - 1; i >= 0; i--) {
     const tr = document.createElement('tr');
     tr.innerHTML = `<td>${i+1}</td>
     <td>${animeList[i].title}</td>
     <td>${animeList[i].episodes}</td>
     <td>${animeList[i].score}</td>
     <td>
-      <button id="btn-update" type="button" class="btn btn-primary">Update</button>
+      <button id="btn-update" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#updateModal">Update</button>
       <button id="btn-remove" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#removeModal">Remove</button>
     </td>`;
     tableBody.appendChild(tr);
   }
 }
 
+// Update modal
+
+
 // Prompt user before removing a row
 tableBody.addEventListener('click', (e) => {
-  if(e.target.classList.contains('btn-danger')) {
-    currentRow = e; 
+  if (e.target.classList.contains('btn-danger')) {
+    currentRow = e;
+  }
+  if (e.target.classList.contains('btn-primary')) {
+    currentRow = e;
   }
 });
 
+// Remove modal buttons
 yesRemoveBtn.addEventListener('click', () => {
   modifyList(currentRow);
   $(removeModal).modal('hide');
@@ -206,13 +233,17 @@ function modifyList(e) {
   e.target.parentElement.parentElement.remove();
 }
 
-// TODO: update button
+// Update modal buttons
+saveUpdateBtn.addEventListener('click', () => {
+  $(updateModal).modal('hide');
+});
 
+cancelUpdateBtn.addEventListener('click', () => {
+  $(updateModal).modal('hide');
+});
+
+// TODO: add seasons, ovas, and movies
 
 // TODO: table pagination
-
-
-// TODO: dropped anime table
-
 
 // TODO: search bar functionality
